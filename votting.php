488ef3ee7f45c4bafe93f9c11ed4a1fb9507db54
Hardian-Developer <64962207+Hardian-Developer@users.noreply.github.com>
2020-05-07 18:24:37 +0700

    Add files via upload

---
+++ votting.php
@@ -0,0 +1,19 @@
+<html>
+	<head>
+	</head>
+	<body>
+		<?php
+			echo "Terimakasih atas Pilihanmu";
+		?>
+		<a href="https://bit.ly/Vote-Cecan-Bagian1" style="display: block; margin: 0 auto; font-size: 18px; font-family: sans-serif; color: dodgerblue;">Kembali</a>
+		<!-- Global site tag (gtag.js) - Google Analytics -->
+		<script async src="https://www.googletagmanager.com/gtag/js?id=UA-145059319-1"></script>
+		<script>
+		window.dataLayer = window.dataLayer || [];
+		function gtag(){dataLayer.push(arguments);}
+		gtag('js', new Date());
+		
+		gtag('config', 'UA-145059319-1');
+		</script>
+	</body>
+</html>
